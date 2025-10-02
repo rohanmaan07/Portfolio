a1db1c2047af6aa6553f65d9e61c75243f3604b7
Rohan Maan <rohanmandal2208@gmail.com>
2025-10-02 16:04:57 +0530

    Contact

--- script.js
+++ script.js
@@ -4,3 +4,39 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const contactForm = document.getElementById('contact-form');
+
+  contactForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const whatsappNumber = '919711657307';
+
+    // Form se data nikalo
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    const services = document.querySelectorAll('input[name="service"]:checked');
+    let selectedServices = [];
+    services.forEach((service) => {
+      selectedServices.push(service.value);
+    });
+    
+  
+    let whatsappMessage = `
+Hello Rohan, I got your number from your portfolio.
+-----------------------------------
+*Name:* ${name}
+*Email:* ${email}
+*Interested Services:* ${selectedServices.join(', ') || 'Not specified'}
+*Project Details:*
+${message}
+-----------------------------------
+    `;
+
+    const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+    window.open(whatsappUrl, '_blank');
+  });
+});
